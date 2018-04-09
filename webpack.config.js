6c55feda2675d2578d272590225b4733f4e38fc0
xiaolin <462441355@qq.com>
2018-04-09 23:12:45 +0800

    * 打包css

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,16 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env','react']
+                        presets: ['env', 'react']
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
         ]
     },
