02bc62e2e2d32d0fdf7a0ba71ef97c8b6b183f5d
xiaolin <462441355@qq.com>
2018-04-13 21:30:41 +0800

    * 格式化webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,116 +1,116 @@
-const path = require('path');
+const path = require('path')
 const merge = require('webpack-merge')
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 var cssParams = [
-    {
-        loader: 'css-loader',
-        options: {
-            minimize: true, // 压缩
-            modules: true
-        }
-    },
-    {
-        loader: 'postcss-loader',
-        options: {
-            ident: 'postcss',
-            plugins: [
-                require('autoprefixer')(), // css代码补全
-                require('postcss-cssnext')()
-            ]
-        }
+  {
+    loader: 'css-loader',
+    options: {
+      minimize: true, // 压缩
+      modules: true
     }
-];
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      ident: 'postcss',
+      plugins: [
+        require('autoprefixer')(), // css代码补全
+        require('postcss-cssnext')()
+      ]
+    }
+  }
+]
 
 module.exports = {
-    entry: './src/app.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: 'js/[name].[hash].js'
-    },
-    devServer: {
-        // 端口号
-        // props: 8086
-    },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.(jsx|js)$/,
-                loader: 'eslint-loader',
-                exclude: [
-                    path.resolve(__dirname, '../node_modules')
-                ]
-            },
-            {
-                test: /\.(jsx|js)$/,
-                // 不处理
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: cssParams
-                })
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: cssParams.concat([{ loader: 'sass-loader' }])
-                })
-            },
-            // 图片
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            // 资源文件夹
-                            name: 'resource/[name].[ext]'
-                        }
-                    }
-                ]
-            },
-            // 字体
-            {
-                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            // 资源文件夹
-                            name: 'resource/[name].[ext]'
-                        }
-                    }
-                ]
-            }
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'js/[name].[hash].js'
+  },
+  devServer: {
+    // 端口号
+    // props: 8086
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(jsx|js)$/,
+        loader: 'eslint-loader',
+        exclude: [
+          path.resolve(__dirname, '../node_modules')
         ]
-    },
-    plugins: [
-        // 复制html文件
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        // 独立css文件
-        new ExtractTextPlugin("css/[name].css"),
-        // 提取公共模块
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            filename: 'js/base.js'
+      },
+      {
+        test: /\.(jsx|js)$/,
+        // 不处理
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: cssParams
         })
-    ],
-};
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: cssParams.concat([{ loader: 'sass-loader' }])
+        })
+      },
+      // 图片
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              // 资源文件夹
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
+      },
+      // 字体
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              // 资源文件夹
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // 复制html文件
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    // 独立css文件
+    new ExtractTextPlugin('css/[name].css'),
+    // 提取公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'js/base.js'
+    })
+  ]
+}
