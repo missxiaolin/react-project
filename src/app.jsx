dda0af2685410f81027a67912d006633d9ed6a6d
xiaolin <462441355@qq.com>
2018-04-14 06:53:19 +0800

    * 修改app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 import 'font-awesome/css/font-awesome.css'
-import './index.css'
 import './index.scss'
 
 // 使用viewport 或者使用 lib-flexible(响应式布局)
 import 'lib-flexible' // eslint-disable-line
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        app
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <div>
-    <h1>Hello, world!</h1>
+    <App />
   </div>,
   document.getElementById('app'),
 )
