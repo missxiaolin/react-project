dfa37a6129499dda9e575869af79aec01ba8cf6c
xiaolin <462441355@qq.com>
2018-04-13 11:24:31 +0800

    * 增加lib-flexible插件

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,9 @@ import 'font-awesome/css/font-awesome.css'
 import './index.css'
 import './index.scss'
 
+// 使用viewport 或者使用 lib-flexible(响应式布局)
+// import 'lib-flexible'
+
 ReactDOM.render(
   <div>
     <i className="fa fa-address-book"></i>
